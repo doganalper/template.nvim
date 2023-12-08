4bd122e953d7da0bb5b9827dd74e5a0402a12bf0
Alper Doğan <alperuni217@gmail.com>
2023-12-08 16:57:06 +0300

    chore(switch_template): print variable

--- lua/template/init.lua
+++ lua/template/init.lua
@@ -7,6 +7,7 @@ function M.switch_template()
   local ft = internals.getFt()
 
   local templates = M.opts.templates[ft]
+  print(#templates)
   if templates ~= nil and #templates > 1 then
     internals.printToBuffer(ft, true)
   end
